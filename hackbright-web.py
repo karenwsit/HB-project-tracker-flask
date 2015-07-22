e3884ccb7fb3de8804b82066c62469b4d4c19ffb
Hackbright Student <info@hackbrightacademy.com>
2015-07-22 12:14:49 -0700

    added student creation form with jinja in html link

--- hackbright-web.py
+++ hackbright-web.py
@@ -21,5 +21,21 @@ def get_student_form():
     """Show form for searching for a student."""
     return render_template("student_search.html")
 
+@app.route('/student-add-new')
+def add_new_student_form():
+  return render_template("new_student.html")  
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+    Fname = request.form.get("fname")
+    Lname = request.form.get("lname")
+    Github = request.form.get("github")
+    hackbright.make_new_student(Fname, Lname, Github)
+    return render_template('new_student_confirm.html', fname=Fname, lname=Lname, github=Github)
+    
+                         
+
+
 if __name__ == "__main__":
     app.run(debug=True)
